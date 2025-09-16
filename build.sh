ef1d4e6fc053331ab7659e38172b20e2e4cb91fb
Roman Vaivod <roman.vaivod@similarweb.com>
2025-09-16 17:27:45 +0200

    add icons

--- build.sh
+++ build.sh
@@ -10,6 +10,10 @@ mkdir -p build
 cp content.js dist/content.js
 cp content.css dist/content.css
 cp manifest.json dist/manifest.json
+mkdir -p dist/icons
+cp icons/icon16.png dist/icons/icon16.png
+cp icons/icon48.png dist/icons/icon48.png
+cp icons/icon128.png dist/icons/icon128.png
 
 # zip dist to build with timestamp
 timestamp=$(date +%Y%m%d%H%M%S)
